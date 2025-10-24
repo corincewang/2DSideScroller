165b084013a41ac5c6df98f1de2d88b32aecdbc5
corinceSub <hanwang3@andrew.cmu.edu>
2025-10-24 12:26:28 -0400

    feat: capsule character move and jump action

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+// based upon the 2DCharacterController from Sharp Coder Blog
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(CapsuleCollider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpHeight = 2f;
+    public float gravityScale = 1f;
+
+    private Rigidbody2D rb;
+    private InputAction moveAction, jumpAction;
+    private float moveDirection;
+    
+    void Start()
+    {
+        // define rigidbody
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.freezeRotation = true;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rb.gravityScale = gravityScale;
+
+        // define actions
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = moveAction.ReadValue<Vector2>().x;
+
+        if (jumpAction.WasPressedThisFrame())
+        {
+            rb.linearVelocityY = jumpHeight;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocityX = moveDirection * speed;
+    }
+}
