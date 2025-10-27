195d7678930bb41bd6c80922bdcf67107de885d2
corinceSub <hanwang3@andrew.cmu.edu>
2025-10-27 13:54:39 -0400

    feat: add basic enemy movement and made environment

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float enemySpeed;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    public bool facingLeft = true;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (facingLeft == sprite.flipX)
+        {
+            sprite.flipX = !facingLeft;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocityX = facingLeft ? -enemySpeed : enemySpeed;
+    }
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "TurnAround")
+        {
+            facingLeft = !facingLeft;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
